2c84609b232c47e39d18cdfa22eb9a1cce7ada0e
lhchar2 <lhchar2@gmail.com>
2016-10-07 21:42:49 +0800

    all servlet The superclass "javax.servlet.http.HttpServlet" was not found on the Java Build Path

--- src/main/java/com/jason/web/controller/HelloController.java
+++ src/main/java/com/jason/web/controller/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
 
 		model.addAttribute("message", "Spring 3 MVC Hello World modifed by jason to office");
 		return "hello";
-
+//
 	}
 
 	@RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
